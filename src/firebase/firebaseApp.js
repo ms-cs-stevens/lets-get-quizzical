6aad1254d7c783a48f45cb1a19d5066d966f1b43
SonaliChavan95 <schava1@stevens.edu>
2021-11-29 12:29:18 -0500

    Connect to firestore and fetch question

--- src/firebase/firebaseApp.js
+++ src/firebase/firebaseApp.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import "firebase/compat/auth";
 import { firebaseConfig } from "../config";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
